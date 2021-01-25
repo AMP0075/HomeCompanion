4b7419220e0fa59a64d88cbc479af2c6c150a967
Ananthu Pillai <ananthum0938@gmail.com>
2021-01-25 20:37:36 +0530

    Add files via upload

---
+++ job-search-app/frontend/src/app.js
@@ -0,0 +1,5 @@
+import { JobSearch } from './JobSearch';
+
+const jobSearch = new JobSearch('#search-form', '.result-container', '.loading-element');
+jobSearch.setCountryCode();
+jobSearch.configureFormListener();
